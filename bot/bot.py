8998132e9d4ad00c7a296bf1608bf3600d19b0cc
timatx242 <tima.tx360@gmail.com>
2025-05-30 13:26:44 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,11 @@
-# bot.py (Telegram bot entry point)
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7845554538:AAEUb8j2dW2f1koma48MuE-CFnlNPmYNwx0")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello, I'm your bot!")
+    await update.message.reply_text("Бот успешно работает!")
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
